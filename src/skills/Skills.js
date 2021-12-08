e2a5e5496e8c19b9b1c08d30dd0be8008ebf894d
Ural <ural-islamov@mail.ru>
2021-12-08 09:34:16 +0500

    first commit

---
+++ src/skills/Skills.js
@@ -0,0 +1,17 @@
+import React from "react";
+import style from './Skills.module.css'
+import styleContainer from '../common/styles/container.module.css'
+
+
+export const Skills = () => {
+    return (
+        <div className={style.skillsBlock}>
+            <div className={style.container}>
+                <h2 className={style.title}>Skills</h2>
+                <div className={style.skills}>
+xzc
+                </div>
+            </div>
+        </div>
+    )
+}
