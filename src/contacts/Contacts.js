a5bf2158a7c9a3f170d0c0c8099ce67e3226795c
Ural <ural-islamov@mail.ru>
2022-03-02 15:18:01 +0500

    add social icons

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -4,6 +4,10 @@ import {Title} from "../common/components/title/Title";
 import phoneIMG from '../assets/img/phone.png'
 import addressIMG from '../assets/img/address.png'
 import emailIMG from '../assets/img/email.png'
+import telegramIMG from '../assets/img/telegram.png'
+import linkedInIMG from '../assets/img/linkedin.png'
+import githubIMG from '../assets/img/github.png'
+import codewarsIMG from '../assets/img/codewars.png'
 import styleContainer from "../common/styles/container.module.css";
 
 export const Contacts = () => {
@@ -30,6 +34,28 @@ export const Contacts = () => {
                         </div>
                         <div className={style.listInner} style={email}>
                             <span>ural-islamov@mail.ru</span>
+                        </div>
+                        <div className={style.socialIcons}>
+                            <div className={style.socialIcon}>
+                                <a target="_blank" href="https://t.me/Ural993">
+                                    <img src={telegramIMG} alt=""/>
+                                </a>
+                            </div>
+                            <div className={style.socialIcon}>
+                                <a target="_blank" href="https://github.com/Ural993">
+                                    <img src={githubIMG} alt=""/>
+                                </a>
+                            </div>
+                            <div className={style.socialIcon}>
+                                <a target="_blank" href="https://www.linkedin.com/in/ural-islamov-289a58160/">
+                                    <img src={linkedInIMG} alt=""/>
+                                </a>
+                            </div>
+                            <div className={style.socialIcon}>
+                                <a target="_blank" href="https://www.codewars.com/users/Ural93">
+                                    <img src={codewarsIMG} alt=""/>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div className={style.formWrapper}>
