bd4c4d1a44975462d9234326bbd72540cd4436fa
Ural <ural-islamov@mail.ru>
2022-03-03 09:13:09 +0500

    add ref

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -20,28 +20,28 @@ export const Nav = () => {
                   className={style.link}
                   spy={true}
                   smooth={true}
-                  offset={-50}
+                  offset={0}
                   duration={500}>Main </Link>
             <Link href="/#" to={'skills'}
                   activeClass={style.activeLink}
                   className={style.link}
                   spy={true}
                   smooth={true}
-                  offset={-20}
+                  offset={1}
                   duration={500}>Skills </Link>
             <Link href="/#" to={'projects'}
                   activeClass={style.activeLink}
                   className={style.link}
                   spy={true}
                   smooth={true}
-                  offset={-20}
+                  offset={1}
                   duration={500}>Projects</Link>
             <Link href="/#" to={'contacts'}
                   activeClass={style.activeLink}
                   className={style.link}
                   spy={true}
                   smooth={true}
-                  offset={-20}
+                  offset={1}
                   duration={500}>Contacts</Link>
         </div>
     )
